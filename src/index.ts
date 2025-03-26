2e2480344ee2cea77b1c35bd780beeb200e52557
Hailey Sanchez <haileysofia2002@gmail.com>
2025-03-26 12:46:20 -0500

    Add swagger docs

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,13 @@ import productRoutes from "./routes/productRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import { errorMiddleware } from "./middleware/errorMiddleware";
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger/swagger_config.json');
+
 dotenv.config();
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(cors());
 app.use(express.json());
 
